73f4a8cd59ad2faf9422e6c49204fb5bfab4a259
Bailey Tieu <baileytieu@ORRHS.ORG>
2020-02-06 13:57:00 -0500

    Intial

---
+++ Lesson1/App.js
@@ -0,0 +1,11 @@
+function setup() {
+    createCanvas(windowWidth, windowHeight)
+    background('lightgray')
+
+    createP('this is peewee herman')
+}
+
+
+function draw() {
+    createP("This is beans")
+}
