535e088e6f6b3ea82ce8e16d1f67294bbc4c843c
Bailey Tieu <baileytieu@ORRHS.ORG>
2020-02-10 12:35:55 -0500

    add event listeners

--- Clicker Game/App.js
+++ Clicker Game/App.js
@@ -3,7 +3,7 @@ let heading
 function setup(){
     createCanvas(windowWidth, windowHeight)
    
-    createImg:   bananaImg = createImg('/banana.png', 'banana image')
+    bananaImg = createImg('/banana.png', 'banana image')
     bananaImg.size(200, 100)
     heading = createElement('h2', 'Click The Banana!')  
 heading.position(width / 3, height / 4)
