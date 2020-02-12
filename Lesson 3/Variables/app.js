22df66cab9cfc445aa71beb4d66c9c4a34ee7b75
Bailey Tieu <baileytieu@ORRHS.ORG>
2020-02-12 10:10:52 -0500

    add event listeners

---
+++ Lesson 3/Variables/app.js
@@ -0,0 +1,26 @@
+// const add = 7 + 3;
+// const sub = 7 - 3;
+// const mult = 7 * 3;
+// const div = 7 / 3;
+// const paren = (83 + 92 + 86 + 94) / 4;
+// console.log(mult)
+
+// let score = 0;
+// let lives = 3;
+// console.log('you have ' +  lives + ' lives left with score: ' + score);
+
+// let points = 1023;
+// let lives = 3;
+// let totalTime = 45;
+// const paren = (1023 * 3) / 45;
+// console.log("Points: " + points);
+// console.log("Lives: " + lives);
+// console.log("Total Time: " + totalTime + " secs");
+// console.log("---------------------------");
+// let totalScore = paren;
+// console.log("Your Total Score is: " + totalScore);
+function setup(){
+let some val = random(0, 100)
+translate(someVal)
+
+}
