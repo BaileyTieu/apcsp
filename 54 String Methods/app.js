2e7ac44c83221f038cac9ee6599f5a2982e76e7c
Bailey Tieu <baileytieu@ORRHS.ORG>
2020-02-27 12:32:44 -0500

    Games

---
+++ 54 String Methods/app.js
@@ -0,0 +1,25 @@
+let s   
+let string = 'my name is Bailey'
+
+// check length
+console.log(string.length)
+
+//conversion
+let num = 10
+num.toString()
+console.log(typeof num)
+
+// searching    
+s = string.search('B')
+s = string.startsWith('f')
+s = string.toLowerCase()
+s = string.concat(" i can nae nae")
+s = string.slice(3, 8)
+s = string.trim()
+console.log(s)
+num = Number(num)
+console.log(typeof num)
+num = num + 
+console.log(num)
+
+
